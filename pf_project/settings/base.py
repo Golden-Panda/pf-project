e3886304e03e70330db2e9072a90f0525bd6f8df
Dokazhi <dosmurat.kazhikumarov@gmail.com>
2018-01-12 23:27:25 +0600

    bug fix

--- pf_project/settings/base.py
+++ pf_project/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wt1v^05^^m^aa1)*$ztnh$n^3&k=-%r+qv0d(&2--qoqm(ok8)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
