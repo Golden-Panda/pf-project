7f65b47fc87bea46c3731ffd5584c97cb212f4c8
Dokazhi <dosmurat.kazhikumarov@gmail.com>
2018-01-12 23:07:07 +0600

    last commit b4 release on heroku

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from django import forms
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from .models import Portfolio, PortfolioImage
+from django.utils.text import slugify
 # Register your models here.
 
 class PortfolioImageInline(admin.TabularInline):
     model = PortfolioImage
     extra=1
 class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','created','modified','link')
+    list_display_links = ('slug',)
+    prepopulated_fields={
+        "slug":("title",)
+    }
+    
     inlines = [
         PortfolioImageInline,
     ]
